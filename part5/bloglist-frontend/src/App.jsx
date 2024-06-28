8ed13ddd84a76361a4e7d9783f39d1e86e7a3f7a
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-06-28 23:04:19 +0530

    ✅Ex: 5.1-5.4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,27 +2,44 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [errorType, setErrorType] = useState(0);
   const [user, setUser] = useState(null);
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newURL, setNewURL] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
   }, []);
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
       setUser(user);
       setUsername("");
       setPassword("");
     } catch (exception) {
-      setError("Wrong credentials");
+      setError("Wrong username or password");
+      setErrorType(0);
       setTimeout(() => {
         setError(null);
       }, 5000);
@@ -30,64 +47,104 @@ const App = () => {
     console.log("logging in with", username, password);
   };
 
-  const loginForm = () => {
-    <form onSubmit={handleLogin}>
-      <div>
-        UserName
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        Password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>;
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser(null);
   };
 
-  if(user===null){
-    return(
+  const addBlog = async (event) => {
+    event.preventDefault();
+    const newBlog = {
+      title: newTitle,
+      author: newAuthor,
+      url: newURL,
+    };
+
+    try {
+      const savedBlog = await blogService.create(newBlog);
+      setBlogs(blogs.concat(savedBlog));
+      setNewTitle("");
+      setNewAuthor("");
+      setNewURL("");
+      setError(`a new blog ${savedBlog.title} by ${savedBlog.author} added`);
+      setErrorType(1);
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    } catch (exception) {
+      setError("Error adding blog");
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+  };
+
+  if (user === null) {
+    return (
       <div>
         <h2>log in to application</h2>
-              <form onSubmit={handleLogin}>
-        <div>
-          UserName
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          Password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-
+        <Notification message={error} type={errorType} />
+        <form onSubmit={handleLogin}>
+          <div>
+            UserName
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            Password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </div>
-    )
+    );
   }
-
   return (
     <div>
       <h2>blogs</h2>
-      {user} logged in <button>logout</button>
+      <Notification message={error} type={errorType} />
+      {user.username} logged in <button onClick={handleLogout}>logout</button>
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={addBlog}>
+          <div>
+            title:
+            <input
+              type="text"
+              value={newTitle}
+              name="newTitle"
+              onChange={({ target }) => setNewTitle(target.value)}
+            />
+            <br />
+            author:
+            <input
+              type="text"
+              value={newAuthor}
+              name="newAuthor"
+              onChange={({ target }) => setNewAuthor(target.value)}
+            />
+            <br />
+            url:
+            <input
+              type="text"
+              value={newURL}
+              name="newURL"
+              onChange={({ target }) => setNewURL(target.value)}
+            />
+            <br />
+            <button type="submit">create</button>
+          </div>
+        </form>
+      </div>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Notification = ({ message, type }) => {
+  const notificationStyle = {
+    color: type === 1 ? "green" : "red",
+    background: "grey",
+    borderWidth: "5px",
+    borderRadius: 5,
+    borderStyle: "solid",
+    borderColor: type === 1 ? "green" : "red",
+    fontSize: 16,
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    paddingLeft: "10px",
+    marginTop: "10px",
+    marginBottom: "10px",
+  };
+  if (message === null) {
+    return null;
+  }
+  return <div style={notificationStyle}>{message}</div>;
+};
+
+export default Notification;
