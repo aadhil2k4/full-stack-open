6aecbb1c353c0e6758d39487191ed74e915fede6
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-07-14 19:39:29 +0530

    Till Ex-6.4

---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,52 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch(action.type){
+    case 'VOTE':{
+      const id = action.payload.id
+      const toChange = state.find(a => a.id === id)
+      const changed = {
+        ...toChange,
+        votes: toChange.votes+1
+      }   
+      return state.map(a => a.id!==id ? a : changed)
+    }
+    case 'NEW_ANECDOTE':{
+      const newAnecdote = asObject(action.payload.content)
+      return [...state, newAnecdote]
+    }
+     default:
+      return state 
+  }
+}
+
+export const addVotes = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
+}
+
+export default reducer
